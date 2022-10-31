0bdfd5f44505ca12774eff9fefc5191939c906c3
Dylan Giles <dylanygiles@gmail.com>
2022-10-31 19:55:49 +1100

    cli module

---
+++ xva/src/cli.rs
@@ -0,0 +1,115 @@
+use serde::Serialize;
+use std::path::Path;
+
+use clap::command;
+use clap::Args;
+use clap::Parser;
+use clap::Subcommand;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Creates a new Xva project.
+    Init(Init),
+}
+
+#[derive(Args, Debug)]
+struct Init {
+    /// The name of the new project. Creates a directory in the current folder for the new project.
+    directory: Option<String>,
+}
+
+pub(crate) fn cli() {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Init(i) => create_project(&i.directory),
+    }
+}
+
+#[derive(Serialize)]
+struct ProjectFile {
+    project: Project,
+}
+
+#[derive(Serialize)]
+struct Project {
+    name: String,
+    version: String,
+}
+
+fn create_project(directory: &Option<String>) {
+    if directory.is_none() {
+        println!("Can't create a Xva project. No directory specified.");
+        return;
+    }
+
+    let full_path = Path::new(std::env::current_dir().unwrap().as_path())
+        .join(Path::new(directory.as_ref().unwrap().as_str()));
+
+    if full_path.exists() {
+        println!(
+            "Can't create a Xva project in {}. The directory already exists.",
+            directory.clone().unwrap()
+        )
+    } else {
+        match std::fs::create_dir(full_path.as_path()) {
+            Ok(_) => match std::fs::write(
+                full_path.join("xvaproj.toml"),
+                toml::to_string(&ProjectFile {
+                    project: Project {
+                        name: directory.as_ref().unwrap().clone(),
+                        version: String::from("0.0.1"),
+                    },
+                })
+                .unwrap()
+                .as_str(),
+            ) {
+                Ok(_) => {}
+                Err(e) => {
+                    println!("Failed to create the project file. {}", e);
+                    return;
+                }
+            },
+            Err(e) => {
+                println!("Failed to create the project directory. {}", e)
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+
+    #[test]
+    fn create_project() {
+        super::create_project(&Some(String::from("project")));
+        let project_file_path = Path::new(std::env::current_dir().unwrap().as_path())
+            .join(Path::new(String::from("project").as_str()))
+            .join(Path::new(String::from("xvaproj.toml").as_str()));
+
+        let file_contents = std::fs::read_to_string(project_file_path).unwrap();
+
+        std::fs::remove_dir_all(
+            Path::new(std::env::current_dir().unwrap().as_path())
+                .join(Path::new(String::from("project").as_str())),
+        )
+        .unwrap();
+
+        assert_eq!(
+            file_contents,
+            r#"[project]
+name = "project"
+version = "0.0.1"
+"#
+        )
+    }
+}
--- xva/src/main.rs
+++ xva/src/main.rs
@@ -1,42 +1,12 @@
-use clap::arg;
-use clap::command;
-use clap::Parser;
-
+mod cli;
 mod compiler;
 mod constants;
 mod machine;
 mod repl;
 mod runtime;
 
+use cli::cli;
+
 fn main() {
     cli();
-}
-
-#[derive(Parser)]
-#[command(version)]
-#[command(name = "Xva")]
-struct Args {
-    input_file: Option<String>,
-
-    /// Indicates the input file is a pre-compiled binary
-    #[arg(short, long)]
-    precompiled: bool,
-}
-
-fn cli() {
-    let args = Args::parse();
-
-    if args.input_file != None {
-        todo!("Compiler")
-        // if args.precompiled {
-        //     execute_precompiled(args.input_file.unwrap());
-        // } else {
-        //     compile_and_execute(args.input_file.unwrap());
-        // }
-    } else {
-        match repl::repl_main() {
-            Ok(_) => {}
-            Err(_) => {}
-        }
-    }
 }
