8a61f0e843967e36dbd56e94fbb1226e1fd425ad
Dylan Giles <dylanygiles@gmail.com>
2023-12-20 19:03:47 +1100

    treesitter boolean literal tests

--- treesitter/bindings/rust/lib.rs
+++ treesitter/bindings/rust/lib.rs
@@ -42,11 +42,57 @@ pub const NODE_TYPES: &'static str = include_str!("../../src/node-types.json");
 
 #[cfg(test)]
 mod tests {
+    use tree_sitter::{Node, Parser};
+
     #[test]
     fn test_can_load_grammar() {
         let mut parser = tree_sitter::Parser::new();
         parser
             .set_language(super::language())
             .expect("Error loading xva language");
+    }
+
+    fn get_parser() -> Parser {
+        let mut parser = tree_sitter::Parser::new();
+        parser
+            .set_language(super::language())
+            .expect("Error loading Xva language");
+        parser
+    }
+
+    /// Extracts nth sibling node at the mth level descendant from the root
+    ///
+    /// `n = 0` is the level immediately descendant of the root.
+    fn extract_nth_node_at_mth_level(root: Node, n: usize, level: usize) -> Node {
+        let mut node: Option<Node> = None;
+        for i in 0..=level {
+            if i == 0 {
+                node = root.child(0)
+            } else {
+                if i == level {
+                    node = node.unwrap().child(n)
+                } else {
+                    node = node.unwrap().child(0)
+                }
+            }
+        }
+
+        node.unwrap()
+    }
+
+    #[test]
+    fn boolean_literal() {
+        let mut parser = get_parser();
+        let tree = parser.parse("true", None).expect("Failed to parse");
+        assert_eq!(
+            extract_nth_node_at_mth_level(tree.root_node(), 0, 2).kind(),
+            "boolean_literal"
+        );
+
+        let tree = parser.parse("false", None).expect("Failed to parse");
+        assert_eq!(
+            extract_nth_node_at_mth_level(tree.root_node(), 0, 2).kind(),
+            "boolean_literal"
+        )
     }
 }
