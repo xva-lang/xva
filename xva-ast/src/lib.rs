c1a07cc3229c98b7807313def00a8a2e769f785d
Dylan Giles <dylanygiles@gmail.com>
2023-12-27 15:23:25 +1100

    some beginner ast types

--- xva-ast/src/lib.rs
+++ xva-ast/src/lib.rs
@@ -1,2 +1,50 @@
-#[cfg(test)]
-mod tests {}
+mod traits;
+
+#[derive(Debug, Clone, Copy)]
+pub struct NodeId(u32);
+
+impl PartialEq for NodeId {
+    fn eq(&self, other: &Self) -> bool {
+        // self.0 == other.0
+        let Self(this) = self;
+        let Self(other) = other;
+        this == other
+    }
+}
+
+impl Eq for NodeId {}
+
+pub struct Brick {
+    items: Vec<Item>,
+}
+
+pub struct Item {
+    id: NodeId,
+    kind: ItemKind,
+}
+
+pub enum ItemKind {
+    Expression(Expression),
+}
+
+pub struct Expression {
+    id: NodeId,
+    kind: ExpressionKind,
+}
+
+pub enum ExpressionKind {
+    Literal,
+}
+
+pub enum LiteralKind {
+    Integer(u128, LiteralIntegerKind),
+    Boolean(bool),
+    Char(char),
+    Float(f64),
+    String(String), // TODO PLEASE change this to interned strings bruh
+}
+
+pub enum LiteralIntegerKind {
+    Signed,
+    Unsigned,
+}
