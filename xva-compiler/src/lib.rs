9c2c43753a3ed11252804f0258d8edc5497cb560
Dylan Giles <dylanygiles@gmail.com>
2024-02-18 19:18:44 +1100

    compiler temporary skeleton

--- xva-compiler/src/lib.rs
+++ xva-compiler/src/lib.rs
@@ -1,12 +1,25 @@
-pub struct CompilerOptions {
-    pub print_cst: bool,
+use std::sync::Arc;
+
+use xva_span::{SourceId, SourceMap};
+pub struct Compiler {
+    source_map: SourceMap,
 }
 
-impl Default for CompilerOptions {
+impl Default for Compiler {
     fn default() -> Self {
         Self {
-            print_cst: Default::default(),
+            source_map: Default::default(),
         }
+    }
+}
+
+impl Compiler {
+    pub fn load_virtual_file(&mut self, name: String, src: String) -> SourceId {
+        self.source_map.load_virtual(name, src)
+    }
+
+    pub fn get_file_content(&self, id: SourceId) -> Option<Arc<str>> {
+        self.source_map.get_raw(id)
     }
 }
 
