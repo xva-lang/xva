e0d5071ffad87e4a540ef067b02c0556c6aa948f
Dylan Giles <dylanygiles@gmail.com>
2024-02-18 19:13:43 +1100

    pub use source::SourceMap

--- xva-span/src/lib.rs
+++ xva-span/src/lib.rs
@@ -1,7 +1,7 @@
 pub(crate) mod monotonic;
 pub mod source;
 
-pub use source::{CheapRange, SourceId, SourceSpan, TokenSpan};
+pub use source::{CheapRange, SourceId, SourceMap, SourceSpan, TokenSpan};
 
 #[cfg(test)]
 mod tests {}
